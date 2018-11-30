d6908015c1f36c260f4a6e43dc74f619db4657d4
Marek Czernek <mczernek@redhat.com>
2018-11-30 12:56:14 +0100

    Add db env

--- src/main/java/guru/springframework/datasources/Datasource.java
+++ src/main/java/guru/springframework/datasources/Datasource.java
@@ -15,9 +15,10 @@ public class Datasource {
         String mysqlHost = System.getenv("MYSQL_SERVICE_HOST");
         String mysqlPort = System.getenv("MYSQL_SERVICE_PORT");
         String formattedHostAndPort = mysqlHost + ":" + mysqlPort;
+        String mysqlDb = System.getenv("MYSQL_DB");
         BasicDataSource bds = new BasicDataSource();
         bds.setDriverClassName("com.mysql.jdbc.Driver");
-        bds.setUrl("jdbc:mysql://" + formattedHostAndPort + "/sampledb");
+        bds.setUrl("jdbc:mysql://" + formattedHostAndPort + "/" + mysqlDb);
         bds.setUsername("root");
         bds.setPassword("root");
         bds.setTestWhileIdle(true);
